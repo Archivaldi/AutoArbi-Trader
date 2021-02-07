eb134236d53e937bd7493b88e7850691ff0c54d4
Archivaldi <artur.markov1860@gmail.com>
2021-02-06 22:18:55 -0600

    deployed to heroku and mysql connected

--- keys.js
+++ keys.js
@@ -1,6 +1,6 @@
 exports.mysql_data = {
-    host: process.env.LOCAL_HOST_NAME,
-    user: 'root',
+    host: process.env.HOST_NAME,
+    user: process.env.USERNAME,
     password: process.env.PASSWORD,
     database: process.env.DATABASE,
     insecureAuth : true
--- server.js
+++ server.js
@@ -5,10 +5,24 @@ var express = require('express');
 var app = express();
 var fs = require('fs');
 
+//keys
+const keys = require('./keys');
+
+//port
+const PORT = process.env.PORT || 8080;
+
 //mysql database connection
 const mysql = require('mysql');
-const connection = mysql.createConnection(keys.mysql_data);
+const connection = mysql.createConnection(process.env.JAWSDB_URL  || keys.mysql_data);
 connection.connect((err)=> {
     if (err){ console.log(err) };
     console.log("Database connected");
+});
+
+app.get("/", (req,res) => {
+    res.send({mesage: "Hello world!"});
+});
+
+app.listen(PORT, () => {
+    console.log(`Listening on ${PORT}`);
 });
