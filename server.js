de09ff6a0ae4ba1d212dd9874d7bb82f3ba6860d
VolksRat71 <nathanieljryan1994@gmail.com>
2021-02-11 17:25:09 -0600

    master branch working

--- server.js
+++ server.js
@@ -41,24 +41,17 @@ const TypingDnaClient = require('typingdnaclient');
 const typingDnaClient = new TypingDnaClient(typingDna_apiKey, typingDna_secret);
 
 //ANVIL api
-const anvilClient = new Anvil({apiKey: keys.anvil.apiKey});
-cloudinary.config({cloud_name: keys.cloudinary.cloud_name, api_key: keys.cloudinary.apikey, api_secret: keys.cloudinary.secret});
+const anvilClient = new Anvil({ apiKey: keys.anvil.apiKey });
+cloudinary.config({ cloud_name: keys.cloudinary.cloud_name, api_key: keys.cloudinary.apikey, api_secret: keys.cloudinary.secret });
 
 
 
 //main page
 app.use(express.static(path.join(__dirname, './client/out')));
 
-
 app.post("/sighup/typingdna", (req, res) => {
-    console.log(req.body)
-    console.log(req.data)
     let typingPattern = req.body.typingPattern;
-    console.log(typingPattern);
-
-app.post("/sighup/typingdna", (req,res) => {
-        let typingPattern = req.body.typingPattern;
-        let client_id = "test123";
+    let client_id = "test123";
 
     typingDnaClient.auto(client_id, typingPattern, (error, response) => {
         if (error) {
@@ -75,7 +68,7 @@ app.post("/sighup/typingdna", (req,res) => {
 
 
 
-app.post("/check_pattern", (req,res) => {
+app.post("/check_pattern", (req, res) => {
     let typingPattern = req.body.typingPattern;
     console.log(typingPattern);
     let client_id = "test123";
@@ -90,59 +83,60 @@ app.post("/check_pattern", (req,res) => {
             if (error) {
                 res.send({ "message": "got some error" });
             } else {
-                res.send({message: "Success!"})
+                res.send({ message: "Success!" })
             };
         });
 });
 
-app.get("/fill_form", async (req,res) => {
+app.get("/fill_form", async (req, res) => {
 
     const exampleData = {
         "title": "Texas Vehicle Bill Of Sale",
         "fontSize": 14,
         "textColor": "#222222",
         "data":
-            {
-                seller: "Artur Markov",
-                sellerStreet: "1403 Fort Lloyd Pl",
-                sellerCity: "Round Rock",
-                sellerState: "TX",
-                sellerZipCode: "78665",
-                sellerCounty: "Williamson",
-                buyer: "Nathaniel Ryan",
-                buyerStreet: "1700 W Parmer St",
-                buyerCity: "Austin",
-                buyerState: "TX",
-                buyerZipCode: "78727",
-                buyer_county: "Jefferson",
-                price: "10000",
-                carYear: "2015",
-                carMake: "Mazda",
-                carBody: "Minivan",
-                carModel: "5",
-                carVin: "1SG13VNSSDN45693",
-                carPlate: "NKR1897",
-                odometer: "77356",
-                dayMonth: moment().format("DD/MM"),
-                year: "21",
-                date: moment().format("MM/DD/YYYY"),
-            }
-      };
+        {
+            seller: "Artur Markov",
+            sellerStreet: "1403 Fort Lloyd Pl",
+            sellerCity: "Round Rock",
+            sellerState: "TX",
+            sellerZipCode: "78665",
+            sellerCounty: "Williamson",
+            buyer: "Nathaniel Ryan",
+            buyerStreet: "1700 W Parmer St",
+            buyerCity: "Austin",
+            buyerState: "TX",
+            buyerZipCode: "78727",
+            buyer_county: "Jefferson",
+            price: "10000",
+            carYear: "2015",
+            carMake: "Mazda",
+            carBody: "Minivan",
+            carModel: "5",
+            carVin: "1SG13VNSSDN45693",
+            carPlate: "NKR1897",
+            odometer: "77356",
+            dayMonth: moment().format("DD/MM"),
+            year: "21",
+            date: moment().format("MM/DD/YYYY"),
+        }
+    };
 
-      const {statusCode, data} = await anvilClient.fillPDF(keys.anvil.bill_of_sale_id, exampleData);
+    const { statusCode, data } = await anvilClient.fillPDF(keys.anvil.bill_of_sale_id, exampleData);
 
     // Data will be the filled PDF raw bytes
-    fs.writeFile('output.pdf', data, { encoding: null }, function(err) {
-        if(err){console.log(err)}
+    fs.writeFile('output.pdf', data, { encoding: null }, function (err) {
+        if (err) { console.log(err) }
         else {
             console.log("the file was created");
 
-            cloudinary.uploader.upload("output.pdf", 
-                function(error, result) {console.log(result, error); });
-        
+            cloudinary.uploader.upload("output.pdf",
+                function (error, result) { console.log(result, error); });
+
+        }
     });
 
-    res.send({statusCode});
+    res.send({ statusCode });
     console.log(data);
 
 
@@ -151,7 +145,7 @@ app.get("/fill_form", async (req,res) => {
 let groupEid = "";
 
 
-app.get("/createEtchSigh", (req,res) => {
+app.get("/createEtchSigh", (req, res) => {
 
     const signer_1_email = "artur.markov1860@gmail.com";
     const signer_1_name = "Artur Markov";
@@ -159,40 +153,40 @@ app.get("/createEtchSigh", (req,res) => {
     const signer_2_email = "archivaldi95@yandex.ru";
     const signer_2_name = "Nate Ryan";
 
-        async function main () {
-            const variables = getPacketVariables()
-            const { data: result } = await anvilClient.createEtchPacket({ variables })
-            const { data, errors } = result
-            if (errors) {
+    async function main() {
+        const variables = getPacketVariables()
+        const { data: result } = await anvilClient.createEtchPacket({ variables })
+        const { data, errors } = result
+        if (errors) {
             console.log('Error', errors)
-            } else {
-                console.log(data.createEtchPacket)
-                groupEid = data.createEtchPacket.documentGroup.eid;
-                console.log("GroupEid :", groupEid);
-            }
+        } else {
+            console.log(data.createEtchPacket)
+            groupEid = data.createEtchPacket.documentGroup.eid;
+            console.log("GroupEid :", groupEid);
         }
+    }
 
-      function getPacketVariables(){
-          return {
-              isDraft: false,
-              isTest: true,
+    function getPacketVariables() {
+        return {
+            isDraft: false,
+            isTest: true,
 
-              name: `Docs - ${signer_2_name}`,
-              signatureEmailSubject: 'Docs ok',
-              signatureEmailBody: 'Please sigh dosc...',
+            name: `Docs - ${signer_2_name}`,
+            signatureEmailSubject: 'Docs ok',
+            signatureEmailBody: 'Please sigh dosc...',
 
-              files: [
-                  {
-                      id: "bill_of_sale",
-                      castEid: keys.anvil.bill_of_sale_id
-                  },
-                  {
-                      id: "texas_title",
-                      castEid: keys.anvil.title_id
-                  }
-              ],
+            files: [
+                {
+                    id: "bill_of_sale",
+                    castEid: keys.anvil.bill_of_sale_id
+                },
+                {
+                    id: "texas_title",
+                    castEid: keys.anvil.title_id
+                }
+            ],
 
-              data: {
+            data: {
                 payloads: {
                     bill_of_sale: {
                         data: {
@@ -236,62 +230,62 @@ app.get("/createEtchSigh", (req,res) => {
                         }
                     }
                 }
-              },
+            },
 
-              signers: [
-                  {
-                      id: "artur",
-                      name: "Artur Markov",
-                      email: "artur.markov1860@gmail.com",
+            signers: [
+                {
+                    id: "artur",
+                    name: "Artur Markov",
+                    email: "artur.markov1860@gmail.com",
 
-                      fields: [
-                          {
-                              fileId: 'bill_of_sale',
-                              fieldId: 'sellerSign'
-                          },
-                          {
-                              fileId: "texas_title",
-                              fieldId: "sellerSign"
-                          }
-                      ]
-                  },
-                  {
-                      id: "nate",
-                      name: "Nate Ryan",
-                      email: "archivaldi95@yandex.ru",
-                      fields: [
-                          {
-                              fileId: "bill_of_sale",
-                              fieldId: "buyerSign"
-                          }
-                      ]
-                  }
-              ]
-          }
-      }
+                    fields: [
+                        {
+                            fileId: 'bill_of_sale',
+                            fieldId: 'sellerSign'
+                        },
+                        {
+                            fileId: "texas_title",
+                            fieldId: "sellerSign"
+                        }
+                    ]
+                },
+                {
+                    id: "nate",
+                    name: "Nate Ryan",
+                    email: "archivaldi95@yandex.ru",
+                    fields: [
+                        {
+                            fileId: "bill_of_sale",
+                            fieldId: "buyerSign"
+                        }
+                    ]
+                }
+            ]
+        }
+    }
 
 
-      function run (fn) {
+    function run(fn) {
         fn().then(() => {
-          //process.exit(0);
-          res.send({message: "Succsess!"})
+            //process.exit(0);
+            res.send({ message: "Succsess!" })
         }).catch((err) => {
-          console.log(err.stack || err.message)
-          process.exit(1)
+            console.log(err.stack || err.message)
+            process.exit(1)
         })
-      }
+    }
 
-      run(main);
+    run(main);
 });
 
 
-app.get("/download", async (req,res) => {
+app.get("/download", async (req, res) => {
 
-    async function main(){
+    async function main() {
         const { statusCode, response, data, errors } = await anvilClient.downloadDocuments(groupEid, {});
-        if (statusCode === 200){
-            fs.writeFile('output.zip', data, { encoding: null }, function(err) {
-                if (err) {console.log(err)}
+        if (statusCode === 200) {
+            fs.writeFile('output.zip', data, { encoding: null }, function (err) {
+                if (err) { console.log(err) }
                 else {
                     console.log("Created zip file");
                 }
@@ -299,13 +293,13 @@ app.get("/download", async (req,res) => {
 
             console.log(statusCode);
         } else {
-            console.log(statusCode,  JSON.stringify(errors, null, 2))
+            console.log(statusCode, JSON.stringify(errors, null, 2))
         };
     }
 
     main()
         .then(() => {
-            res.send({message: "Succsess!"})
+            res.send({ message: "Succsess!" })
         })
         .catch((err) => {
             console.log(err.stack || err.message);
