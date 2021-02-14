7aeb4c76ddabd761511dcc2592518dab4cb829ab
VolksRat71 <nathanieljryan1994@gmail.com>
2021-02-14 13:35:52 -0600

    loading wheel, and api call made!

--- client/src/utils/authSteps.js
+++ client/src/utils/authSteps.js
@@ -3,13 +3,14 @@ import { v4 as uuidv4 } from 'uuid';
 export const authSteps = {
     appScript: {
         landing: "Hello 👋 Welcome to auto arbi-trader, your solution for private vehicle sales mediation! What will you be doing today?",
-        secondAuth: "Great! And what is your email and password?"
+        secondAuthSignUp: "😊 Nice to meet you! May I get your email, password, & role in this transaction?",
+        secondAuthLogin: "🥳 Welcome Back, we are happy to have you! May I get your email & password?"
     },
     message: [
         "I am authenticated, by the way I type.",
     ],
     route: {
-        signUp: '/api/typing-dna/signup',
+        signUp: '/api/db/signup',
         verify: '/api/typing-dna/verify'
     },
     userId: uuidv4()
