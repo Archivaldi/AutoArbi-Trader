fe5c197dc8cf8ea7658a0b2a222078f178a9837f
Archivaldi <artur.markov1860@gmail.com>
2021-02-16 22:43:11 -0600

    delete acocunt

--- controllers/api/typing-dna-routes.js
+++ controllers/api/typing-dna-routes.js
@@ -3,6 +3,8 @@ const TypingDnaClient = require('typingdnaclient');
 const keys = require('../../config/keys');
 const { typingDna_apiKey, typingDna_secret } = keys.typingDna;
 const typingDnaClient = new TypingDnaClient(typingDna_apiKey, typingDna_secret);
+const connection = require("../../config/db");
+
 router.post("/check-pattern", (req, res) => {
     const { typingPattern, user_id, role } = req.body;
     typingDnaClient.auto(
@@ -26,4 +28,30 @@ router.post("/check-pattern", (req, res) => {
             };
         });
 });
+
+
+router.get("/destroyAccount", (req,res) => {
+    const {user_id, role} = req.session;
+    let query = "";
+
+    if (role === "buyer"){
+        query = "DELETE FROM Users WHERE user_id = ?"
+    } else if (role === "seller"){
+        query = "DELETE Users, Cars FROM Users inner JOIN Cars USING (car_id) where user_id = ?"
+    };
+
+    connection.query(query, [user_id], (err, result) => {
+        if (err) throw err;
+        else {
+            typingDnaClient.delete({userId: user_id}, (err, result) => {
+                if (err) throw err;
+                else {
+                    console.log(result);
+                    req.session = null;
+                }
+            });
+        }
+    });
+});
+
 module.exports = router;
